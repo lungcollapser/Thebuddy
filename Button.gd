729e3f729a771a40168ba60f03d14635907ab302
lungcollapser <milesterrell1337@gmail.com>
2024-08-06 08:37:06 -0500

    Mouse Cursor work

--- Button.gd
+++
@@ -1,9 +0,0 @@
-extends Button
-
-var hey = "hey"
-
-
-
-func _on_pressed():
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-		print("hey")
--- Cursor.gd
+++ Cursor.gd
@@ -1,1 +1,13 @@
+extends Node2D
+
+var cursor1 = preload("res://Sprites/Aseprite/UI/Cursor/cursor1.png")
+var cursor2 = preload("res://Sprites/Aseprite/UI/Cursor/cursor2.png")
+
+
+
+func _on_pressed():
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		Input.set_custom_mouse_cursor(cursor2, Input.CURSOR_ARROW, Vector2(0,0))
+	else: 
+		Input.set_custom_mouse_cursor(cursor1, Input.CURSOR_ARROW, Vector2(0,0))
 
