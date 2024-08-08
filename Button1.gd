bd8b38dfababe9203a7462f7f584b6313c3a35ad
lungcollapser <milesterrell1337@gmail.com>
2024-08-07 21:07:27 -0500

    Pushpushbabay

---
+++ Button1.gd
@@ -0,0 +1,9 @@
+extends Node2D
+var buddy = "res://character_body_2d.gd"
+const JUMP = 100 
+
+
+
+
+func _on_button_pressed():
+	pass
---
+++ character_body_2d.gd
@@ -0,0 +1,11 @@
+extends CharacterBody2D
+const JUMP_VELOCITY = 150
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	move_and_slide()
