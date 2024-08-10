be1a308b8688d6a83708d00d9679bac959ed8b74
lungcollapser <milesterrell1337@gmail.com>
2024-08-10 10:22:05 -0500

    Button2

--- Button2.gd
+++ Button2.gd
@@ -1,12 +1,6 @@
 extends Node2D
 
 #Code for activating the button. Works using signal from collisionshape2D
-func _on_button_area_input_event(viewport, event, shape_idx):
+func _on_area_2d_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
-		%Buddy1Body._jump_up()
-		
-
-
-func _on_button_area_mouse_entered():
-	%Buddy1Body._jump_up()
-	
+		%Buddy1Body.visible = false
--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,19 +1,19 @@
 extends CharacterBody2D
 const JUMP_VELOCITY = -150
 const SPEED = 100
+var hunger = 100
 
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
-
+#Adds the gravity.
 func _physics_process(delta):
-	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 		
 	move_and_slide()
-
+#Adds a jumop function to the buddy(mainly for testing).
 func _jump_up(): 
 	if is_on_floor():
 		velocity.y += JUMP_VELOCITY
