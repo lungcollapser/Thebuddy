55460b3aedfac84186823f230d596fa2d66b96d3
lungcollapser <milesterrell1337@gmail.com>
2024-08-09 19:59:49 -0500

    Button

--- Button1.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-var buddy = "res://character_body_2d.gd"
-const JUMP = 100 
-
-
-
-
-func _on_button_pressed():
-	pass
--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,11 +1,20 @@
 extends CharacterBody2D
 const JUMP_VELOCITY = 150
+const SPEED = 100
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
 
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		
+	move_and_slide()
+
+func _on_button_pressed(JUMP): 
+	velocity.y = JUMP_VELOCITY * SPEED 
+
+
 
 	move_and_slide()
