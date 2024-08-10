ecc08b6380a92f56363ef6a68de7b2b3ee4f27d7
cinnycoke <156374944+cinnycoke@users.noreply.github.com>
2024-08-09 19:58:39 -0500

    Buddywork
    AAAAAAAAAAAAAAAA

--- Cursor.gd
+++ Cursor.gd
@@ -3,14 +3,8 @@ extends Node2D
 var cursor1 = preload("res://Sprites/UI/cursor1.png")
 var cursor2 = preload("res://Sprites/UI/cursor2.png")
 
-## Anim was not working so I changed functions
-## There is a method to scaling pixel art using "nearest neighbor" that keeps it crispy but I couldnt figure it out
-
 func _process(delta):
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 		Input.set_custom_mouse_cursor(cursor2, Input.CURSOR_ARROW, Vector2(0,0))
 	else: 
 		Input.set_custom_mouse_cursor(cursor1, Input.CURSOR_ARROW, Vector2(0,0))
-
-
-
