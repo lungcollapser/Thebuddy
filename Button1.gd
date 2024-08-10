31a5f764f8317f0923f31db518121cbb45bc85bb
lungcollapser <milesterrell1337@gmail.com>
2024-08-09 21:36:06 -0500

    pushpush

--- Button1.gd
+++ Button1.gd
@@ -1,7 +1,15 @@
 extends Node2D
+@onready var cursor_area = $CursorArea
 
 #Code for activating the button. Works using signal from collisionshape2D
 func _on_button_area_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		%Buddy1Body._jump_up()
 		
+
+func _on_button_area_area_entered(area):
+		pass		
+
+
+func _on_button_area_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
+	pass # Replace with function body.
