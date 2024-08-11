d7d5f4d626aa69be06683c5426bcb606b363e5b4
lungcollapser <milesterrell1337@gmail.com>
2024-08-10 19:49:53 -0500

    boobie

--- Button1.gd
+++ Button1.gd
@@ -1,5 +1,7 @@
 extends Node2D
-
+var cursor1 = preload("res://Sprites/UI/cursor1.png")
+var cursor2 = preload("res://Sprites/UI/cursor2.png")
+var cursor_node = preload("res://Cursor.gd")
 #Code for activating the button. Works using signal from collisionshape2D
 func _on_button_area_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
@@ -8,5 +10,7 @@ func _on_button_area_input_event(viewport, event, shape_idx):
 
 
 func _on_button_area_mouse_entered():
-	%Buddy1Body._jump_up()
+	if cursor1 == cursor1:
+		Input.set_custom_mouse_cursor(cursor2, Input.CURSOR_ARROW, Vector2(0,0))
+		
 	
