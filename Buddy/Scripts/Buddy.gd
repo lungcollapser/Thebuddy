acb40c1338fc569625de70353fadb114b458018d
lungcollapser <milesterrell1337@gmail.com>
2024-08-20 22:41:49 -0500

    Hungerbarfinished

--- Buddy/Scripts/Buddy.gd
+++ Buddy/Scripts/Buddy.gd
@@ -1,11 +1,11 @@
 extends CharacterBody2D
 const JUMP_VELOCITY = -150
 const SPEED = 100
-var hunger_meter = 5
+var hunger_meter = 30
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
-#Adds the gravity.
+#Adds the gravity to the scene.
 func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -15,10 +15,11 @@ func _physics_process(delta):
 func _jump_up(): 
 	if is_on_floor():
 		velocity.y += JUMP_VELOCITY
-	
-
 	move_and_slide()
-	
+
+#-------------------------------------------------------------------------------
+
+
 #Function for feeding the buddy. Clamp allows it to have a maximum value of 10. 
 #The print is for testing
 func feed_buddy():
@@ -28,12 +29,27 @@ func feed_buddy():
 	%Thanks.visible = true
 	await get_tree().create_timer(2).timeout
 	%Thanks.visible = false
-#Function for the timer to reduce the hunger meter by 1 after 1 second. 
-#The print is for testing. It also dsiplays different text after two second intervals.	
-func _on_timer_timeout():
+
+#This is here to make sure the hunger bar animation works smoothly
+func _process(_delta):
+	set_hunger_bar()
+
+#This is to match the hunger bar value with the hunger_meter variable as it decreases.
+func set_hunger_bar():
+	%HungerBar.value = hunger_meter 
+
+#This is a seperate lose hunger function. We call this at the end of _on_timer_timeout to reduce the hunger overtime and show the labels.
+#The print is for testing.
+func lose_hunger():
 	hunger_meter -= 1
 	hunger_meter = clamp(hunger_meter, 0, 30)
 	print(hunger_meter)
+	set_hunger_bar()
+	
+#Function for the timer to reduce the hunger meter by 1 after 1 second. 
+#The print is for testing. It also dsiplays different text after two second intervals.
+#THIS IS LIKELY TEMPORARY CODE FOR TESTING 
+func _on_timer_timeout():
 	if hunger_meter == 20:
 		%Hunger75.visible = true
 		await get_tree().create_timer(2).timeout
@@ -48,7 +64,8 @@ func _on_timer_timeout():
 		%Hunger25.visible = false
 	elif hunger_meter == 0:
 		%Dead.visible = true
-			
-		
-		
+	
+	lose_hunger()
+	
+
 	
