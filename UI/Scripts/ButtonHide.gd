f20a52e2291143b15292848a564f2ae35fc90e3d
lungcollapser <milesterrell1337@gmail.com>
2024-08-29 15:22:04 -0500

    feedbuddy&xbutton

--- UI/Scripts/ButtonHide.gd
+++ UI/Scripts/ButtonHide.gd
@@ -4,6 +4,7 @@ extends BounceCursor
 func _on_area_2d_input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		%Buddy1Body.feed_buddy()
+		
 func _on_area_2d_mouse_entered():
 	bounce_start()
 		
