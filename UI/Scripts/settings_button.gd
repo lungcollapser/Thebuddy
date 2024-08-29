c881eef69bf162a1816e4256cd20483e246553b5
lungcollapser <milesterrell1337@gmail.com>
2024-08-29 13:47:24 -0500

    anguish

--- UI/Scripts/settings_button.gd
+++ UI/Scripts/settings_button.gd
@@ -3,6 +3,7 @@ extends BounceCursor
 func _on_settings_button_area_input_event(_viewport: Node, event: InputEvent, _shape_idx: int):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		%Settings.visible = true
+		%XButton.visible = true
 
 		
 
--- UI/x_button.gd
+++
@@ -1,5 +0,0 @@
-extends BounceCursor
-
-func _on_x_button_area_input_event(_viewport: Node, event: InputEvent, _shape_idx: int):
-		if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
-			%Settings.visible = false
