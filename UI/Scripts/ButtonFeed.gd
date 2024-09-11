41c7b6964ad2ef680d7d4a0551aa98199cdf265c
lungcollapser <milesterrell1337@gmail.com>
2024-09-11 07:45:59 -0500

    testing for hunger button
    test code is commented. work on it more tonight

--- UI/Scripts/ButtonFeed.gd
+++ UI/Scripts/ButtonFeed.gd
@@ -4,6 +4,10 @@ extends BounceCursor
 func _on_area_2d_input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		%Buddy1Body.feed_buddy()
+	#if Input.is_action_just_pressed("toggle_disabled"):
+		#%Budd1Body.feed_buddy() == false
+		#await get_tree().create_timer(20).timeout
+		#%Budd1Body.feed_buddy() == true
 		
 func _on_area_2d_mouse_entered():
 	bounce_start()
