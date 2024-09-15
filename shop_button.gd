4d781fa5241bf4044d78bf3a9ecb8de3dbced11e
lungcollapser <milesterrell1337@gmail.com>
2024-09-15 18:45:54 -0500

    shop implementation

---
+++ shop_button.gd
@@ -0,0 +1,15 @@
+extends BounceCursor
+
+
+func _on_shop_button_area_mouse_entered():
+	bounce_start()
+
+
+func _on_shop_button_area_mouse_exited():
+	bounce_stop()
+
+
+
+func _on_shop_button_area_input_event(_viewport: Node, event: InputEvent, _shape_idx: int):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		get_tree().change_scene_to_file("res://UI/ShopScene.tscn")
