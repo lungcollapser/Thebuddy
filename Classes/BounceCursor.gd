1b7c6407a1eaecb8b715df753e87f5778e092601
lungcollapser <milesterrell1337@gmail.com>
2024-09-16 11:52:06 -0500

    autoload
    made cursor a global variable so it can be reached from different scenes. doesn't work with current buttons rn but gonna figure that out

--- Classes/BounceCursor.gd
+++ Classes/BounceCursor.gd
@@ -2,9 +2,9 @@ extends Node2D
 class_name BounceCursor
 
 func bounce_start():
-	%Cursor.cursor_hovering = true	
-	if %Cursor.anim_timer == true: ## Checks that bounce_cursor is not already running
-		%Cursor.bounce_cursor()
+	Cursor.cursor_hovering = true	
+	if Cursor.anim_timer == true: ## Checks that bounce_cursor is not already running
+		Cursor.bounce_cursor()
 		
 func bounce_stop():
-	%Cursor.cursor_hovering = false
+	Cursor.cursor_hovering = false
--- UI/Scripts/ButtonPoke.gd
+++ UI/Scripts/ButtonPoke.gd
@@ -5,8 +5,11 @@ func _on_button_area_input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		%Buddy1Body._jump_up()
 		
-func _on_button_area_mouse_entered():
+		
+func _on_poke_button_area_mouse_entered():
 	bounce_start()
 	
-func _on_button_area_mouse_exited():
+	
+
+func _on_poke_button_area_mouse_exited():
 	bounce_stop()
