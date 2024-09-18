4ea96e63208f6fb906710a74169a287afd0a75f7
lungcollapser <milesterrell1337@gmail.com>
2024-09-17 23:56:48 -0500

    nathanforretards

--- UI/Scripts/Cursor.gd
+++ UI/Scripts/Cursor.gd
@@ -1,5 +1,13 @@
 extends Node2D
 
+#@export var cursor_default = ViewportTexture
+#@export var cursor_click = ViewportTexture
+#@export var cursor_anim = ViewportTexture
+
+signal call_bounce_start
+signal call_bounce_stop
+
+
 var cursor_default = preload("res://Sprites/UI/cursor1.png")
 var cursor_click = preload("res://Sprites/UI/cursor2.png")
 var cursor_anim = preload("res://Sprites/UI/cursor_anim.png")
@@ -28,4 +36,25 @@ func bounce_cursor():
 		await get_tree().create_timer(0.25).timeout ## 0.25 second timer
 	anim_timer = true ## Allows bounce_cursor to be ran again in _physics_process
 	## "if" loops are separate to prevent jittery anim when LMB is pressed during animation
+	
+
+
+func _on_call_bounce_start():
+	bounce_start()
+	print("cock")
+	
+	
+func _on_call_bounce_stop():
+	bounce_stop()
+	print("cock")
+
+
+func bounce_start():
+	Cursor.cursor_hovering = true	
+	if Cursor.anim_timer == true: ## Checks that bounce_cursor is not already running
+		Cursor.bounce_cursor()
 		
+func bounce_stop():
+	Cursor.cursor_hovering = false
+	
+	
--- UI/Scripts/settings_button.gd
+++ UI/Scripts/settings_button.gd
@@ -1,16 +1,18 @@
-extends BounceCursor
+extends TextureButton
 
 func _on_settings_button_area_input_event(_viewport: Node, event: InputEvent, _shape_idx: int):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		%Settings.visible = true
 		%XButton.visible = true
 
-		
 
 
-func _on_settings_button_area_mouse_entered(): 
-	bounce_start()
+func _on_mouse_entered():
+	Cursor.call_bounce_start
+	
 
 
-func _on_settings_button_area_mouse_exited(): 
-	bounce_stop()
+func _on_mouse_exited():
+	Cursor.call_bounce_stop
+	
+	
