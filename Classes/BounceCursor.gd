e12abaef537c1dfb8a23f282a7484350c547ccac
lungcollapser <milesterrell1337@gmail.com>
2024-09-17 20:43:43 -0500

    goofin with cursor
    still need to fix idk why tf its not working but ill get it

--- Classes/BounceCursor.gd
+++ Classes/BounceCursor.gd
@@ -2,9 +2,9 @@ extends Node2D
 class_name BounceCursor
 
 func bounce_start():
-	Cursor.cursor_hovering = true	
-	if Cursor.anim_timer == true: ## Checks that bounce_cursor is not already running
-		Cursor.bounce_cursor()
+	GlobalCursor.cursor_hovering = true	
+	if GlobalCursor.anim_timer == true: ## Checks that bounce_cursor is not already running
+		GlobalCursor.bounce_cursor()
 		
 func bounce_stop():
-	Cursor.cursor_hovering = false
+	GlobalCursor.cursor_hovering = false
--- UI/Scripts/ShopBackButton.gd
+++ UI/Scripts/ShopBackButton.gd
@@ -1,6 +1,6 @@
 extends BounceCursor
 
-func _on_shop_back_button_area_input_event(viewport: Node, event: InputEvent, shape_idx: int):
+func _on_shop_back_button_area_input_event(_viewport: Node, event: InputEvent, _shape_idx: int):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		get_tree().change_scene_to_file("res://BuddyGame.tscn")
 		
