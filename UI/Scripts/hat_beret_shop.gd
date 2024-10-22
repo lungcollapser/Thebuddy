a092a642e5e4a161434dcb261e8b6031ff317872
lungcollapser <milesterrell1337@gmail.com>
2024-10-22 12:57:25 -0500

    beret animation fixed up

--- UI/Scripts/hat_beret_shop.gd
+++ UI/Scripts/hat_beret_shop.gd
@@ -1,10 +1,5 @@
 extends TextureButton
 
-var buddy
-@onready var hat_beret = $HatBeretShop.instatiate()
-
-func _ready() -> void:
-	buddy = get_tree().get_nodes_in_group("Buddy")
 
 func _on_mouse_entered():
 	Events.emit_signal("call_bounce_start")
@@ -13,7 +8,3 @@ func _on_mouse_entered():
 
 func _on_mouse_exited():
 	Events.emit_signal("call_bounce_stop")
-
-
-
-func _on_button_down():
