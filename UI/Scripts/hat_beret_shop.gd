da1179425764517d31ac51cc0b290854fbd643c4
lungcollapser <milesterrell1337@gmail.com>
2024-10-21 19:05:54 -0500

    inventoryscene

--- UI/Scripts/hat_beret_shop.gd
+++ UI/Scripts/hat_beret_shop.gd
@@ -1,5 +1,10 @@
 extends TextureButton
 
+var buddy
+@onready var hat_beret = $HatBeretShop.instatiate()
+
+func _ready() -> void:
+	buddy = get_tree().get_nodes_in_group("Buddy")
 
 func _on_mouse_entered():
 	Events.emit_signal("call_bounce_start")
@@ -8,3 +13,7 @@ func _on_mouse_entered():
 
 func _on_mouse_exited():
 	Events.emit_signal("call_bounce_stop")
+
+
+
+func _on_button_down():
