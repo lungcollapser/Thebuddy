9ae6dd66b809567d79434803b2ccce4df94b45a5
lungcollapser <milesterrell1337@gmail.com>
2024-11-09 19:34:21 -0600

    fixed some code errors
    in inventory

--- UI/grid_container.gd
+++ UI/grid_container.gd
@@ -8,28 +8,20 @@ var hat_buddy_check = false
 
 
 
+func _ready() -> void:
+	hat_beret_buddy = get_tree().get_nodes_in_group("HatBeretBuddy")[0]
+	hat_bowl_buddy = get_tree().get_nodes_in_group("HatBowlBuddy")[0]
 
 func _on_hat_beret_inventory_button_down():
-	if hat_buddy_check == false:
-		hat_buddy_check = true
-		hat_beret_buddy.visible = true
-		hat_bowl_buddy.visible = false
-	elif hat_buddy_check == true:
-		hat_buddy_check = false
-		hat_beret_buddy.visible = false
-		
+	hat_beret_buddy.visible = true
 	
 
 
 
 func _on_hat_bowl_inventory_button_down():
-	if hat_buddy_check == false:
-		hat_buddy_check = true
-		hat_bowl_buddy.visible = true
-		hat_beret_buddy.visible = false
-	elif hat_buddy_check == true:
-		hat_buddy_check = false
-		hat_bowl_buddy.visible = false
+	hat_bowl_buddy = true
+	
+		
 
 
 #Possible fix for these code conditions. Test later
